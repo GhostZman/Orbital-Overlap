1c8c14970e508a600c8249a923e04d6b6ad6fde8
Zachary Ives <zives@hawk.iit.edu>
2024-02-28 13:29:16 -0600

    Initial Commit

---
+++ Orbital Overlap/Orbital_OverlapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orbital_OverlapApp.swift
+//  Orbital Overlap
+//
+//  Created by Phys440Zachary on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Orbital_OverlapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Orbital OverlapUITests/Orbital_OverlapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Orbital_OverlapUITestsLaunchTests.swift
+//  Orbital OverlapUITests
+//
+//  Created by Phys440Zachary on 2/28/24.
+//
+
+import XCTest
+
+final class Orbital_OverlapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
